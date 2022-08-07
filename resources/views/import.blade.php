f9c1b12cdc266fb769b0f00b486b3481aa3d0b30
Lance Ronquillo <ronquillolance@gmail.com>
2022-08-08 00:52:06 +0800

    header & footer

---
+++ resources/views/import.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Baskervville&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/d9759e454e.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ resources/views/marginals.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="{{ url('css/marginals.css') }}">
+</head>
+
+<body>
+    <header>
+        <nav class="userHeader navbar navbar-expand-lg navbar-dark d-block sticky-top position-relative">
+            <div class="container-lg">
+                <a href="#" class="navbar-brand mx-3">
+                    <img src="{{ url('img/nall_logo.png') }}" alt="NALL Logo" style="width:3rem;">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler"
+                    aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+
+                <div class="collapse navbar-collapse mx-lg-0 mx-3" id="navbarToggler">
+                    <ul class="navbar-nav">
+                        <li class="nav-item my-lg-0 ms-lg-3 mt-2 me-3">
+                            <a href="" class="nav-link">Explore</a>
+                        </li>
+                        <li class="nav-item my-lg-0 me-3 mt-2">
+                            <a href="" class="nav-link">About Us</a>
+                        </li>
+                        <li class="nav-item my-lg-0 me-3 mt-2">
+                            <a href="" class="nav-link">Contact Us</a>
+                        </li>
+
+                    </ul>
+                    <ul class="navbar-nav ms-auto my-lg-0 me-0 mt-2 justify-content-end">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                My Account
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end dropdownAccount m-0 p-0 border-0">
+                                <li class="p-0"><a href="#" class="dropdown-item text-light py-2 px-4">Settings
+                                        & Privacy</a></li>
+                                <li class="p-0"><a href="#" class="dropdown-item text-light py-2 px-4">Help &
+                                        Support</a></li>
+                                <li class="p-0"><a href="#"
+                                        class="dropdown-item text-light py-2 px-4">Logout</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="nallHeader text-light mx-auto py-3">
+            <div class="container-lg d-flex justify-content-center">
+                <img src="{{ url('img/nall_logo.png') }}" class="mx-2" alt="NALL Logo" style="width:3rem;">
+                <div class="nallHeader-title mx-2 my-auto">
+                    Network of Academic<br>Law Librarians, Inc.
+                </div>
+            </div>
+        </div>
+    </header>
+    <footer>
+        <div class="userFooter fixed-bottom text-light text-center py-3">
+            <p class="p-0 m-0">©2022 A Gateway to Open Access Legal<br class="d-lg-none d-block"> Resources in the
+                Philippines All rights reserved.</p>
+        </div>
+    </footer>
+</body>
+
+</html>
