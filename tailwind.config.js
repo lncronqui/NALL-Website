ad3488f4460604d62160034f5f78da51271f2bc7
Ma. Pauline Yvana Amores <paulineyvana23@gmail.com>
2022-10-20 23:50:56 +0800

    Pau Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const colors = require('tailwindcss/colors')
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
@@ -11,9 +12,18 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Open_Sans', ...defaultTheme.fontFamily.sans],
             },
         },
+        colors: {
+            primary: {
+              50: '#FFFFFF',
+              100: '#FF29FB',
+              200: '#9F329D',
+              300: '#2E052D',
+              400: '#000000'
+            },
+          }
     },
 
     plugins: [require('@tailwindcss/forms')],
