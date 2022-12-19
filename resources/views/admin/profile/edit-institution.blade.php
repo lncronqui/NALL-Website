beb8442de723a12bb092bc1615be3a234149fd04
kaiririri <kyliediz1601@gmail.com>
2022-12-20 04:05:06 +0800

    Update edit-institution.blade.php

--- resources/views/admin/profile/edit-institution.blade.php
+++ resources/views/admin/profile/edit-institution.blade.php
@@ -28,11 +28,12 @@
                         <div class="flex flex-row gap-x-24">
                             <x-label for="text" :value="__('New Institution')" class="font-normal mt-3"
                                 style="color:#383838; font-size: 1rem;" />
-                            <div class="ml-6">
-                                <select required name="institution_id" id="institution_id" class="shadow appearance-none border rounded h-10 py-2 pl-4 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" style="width: 31vw;">
-                                    <option value="" selected disabled>--Select Institution--</option>
-                                </select>
-                            </div>
+                                <select name="format" class="shadow appearance-none border rounded py-2 px-3"
+                                style="width:348px; margin-left:23px; height: 45px;">
+                                    <option selected disabled>---Select Institution---</option>
+                                    <option value="#">Option 1</option>
+                                    <option value="#">Option 2</option>
+                            </select>
                         </div>
                     </div>
 
