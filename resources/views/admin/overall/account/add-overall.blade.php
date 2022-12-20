42602999c2b7f18637edd1c8c253f2a12c1483fc
Gabruwu <70462340+G4bruu@users.noreply.github.com>
2022-12-20 21:32:33 +0800

    Add Admin  (no routing?)

--- resources/views/admin/overall/account/add-overall.blade.php
+++ resources/views/admin/overall/account/add-overall.blade.php
@@ -1,6 +1,35 @@
 <link href="/css/tableprinted.css" rel="stylesheet" type="text/css" />
+<link href="/css/tableprinted.css" rel="stylesheet" type="text/css"/>
+<x-admin.layout>
+    <div
+        class="grid-container grid lg:grid-cols-10 md:grid-cols-10 divide-x-4 divide-solid divide-gray-300 pt-10 pb-20 px-10">
 
-<div class="print">
+        <div class="text-left col-span-2">
+            <x-admin.side-nav></x-admin.side-nav>
+            <script>
+                const myElement = document.getElementById("useradmin-list");
+                myElement.style.color = "#2E052D";
+                myElement.style.fontWeight = "500";
+            </script>
+        </div>
+
+        <div class="text-left col-span-8">
+            <div class="border-b-2 border-gray-300 ml-16 mt-5 mr-16">
+                <h1 class="text-3xl font-extrabold mb-3 ml-2 text-center">Add Admin</h1>
+            </div>
+            <div class="ml-16 mt-3 mr-16 mb-10">
+                <a style="font-size:24px" class="fa" style="color: #C4C4C4;" href="{{ route('admin.overall.accounts.index') }}">&#xf060;</a>
+                <h1 class="text-2xl mt-2 font-bold ml-2 text-center" style="margin-top:-20px;">Type </h1>
+                <div class="border-b-2 border-gray-300">
+                    <div class="select2 mt-2 mb-4 ml-20 mr-24">
+                        <select name="format" id="editFormat" onchange="siteRedirect()">
+                            <option selected disabled>Overall Admin</option>
+                            <option value="{{ route('admin.repository.create.uni') }}">University Admin</option>
+
+                        </select>
+                    </div>
+                </div>
+<div class="overall">
     <div class="ml-20 mr-20 mt-6 mb-6">
         <div class="border-b-2 border-gray-300">
             <div class="ml-16 mr-16">
@@ -63,5 +92,15 @@
         </div>
         </form>
 
-    </div>
 </div>
+<script>
+
+function siteRedirect() {
+var selectbox = document.getElementById("editFormat");
+var selectedValue = selectbox.options[selectbox.selectedIndex].value;
+window.location.href = selectedValue;
+}
+</script> 
+
+</div></div></div>
+</x-admin.layout>
