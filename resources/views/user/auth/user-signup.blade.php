8943394b86a57357f717bc958e788866373c885b
Ma. Pauline Yvana Amores <paulineyvana23@gmail.com>
2022-12-21 13:23:48 +0800

    fixed margins

--- resources/views/user/auth/user-signup.blade.php
+++ resources/views/user/auth/user-signup.blade.php
@@ -109,13 +109,35 @@
 
 
 <!-- The Modal -->
-<div id="myModal" class="modal">
+<div id="myModal" class="modal2">
 
   <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-    <p>Some text in the Modal..</p>
-  </div>
+  <div class="modal2-content">
+                                <span class="close">&times;</span>
+                                    <div class="term-head text-center text-xl mt-2" style="font-weight: 500;">
+                                        <h1>Terms and Conditions</h1>
+                                    </div>
+
+                                    <!-- terms and conditions content -->
+                                    <div class="term-head text-justify text-lg mt-5 ml-10 mr-10 mb-5" style="font-weight: 300;">
+                                        <p>"<b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                                            incididunt ut labore et dolore magna aliqua.</b> Ut enim ad minim veniam, quis nostrud 
+                                            exercitation ullamco laboris nisi ut aliquip ex <br><br> ea commodo consequat. Duis aute 
+                                            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
+                                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                                            deserunt mollit anim id est laborum.""Sed ut perspiciatis unde omnis iste natus error 
+                                            sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae 
+                                            ab illo inventore veritatis et quasi architecto <br><br> beatae vitae dicta sunt explicabo. 
+                                            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+                                             consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro 
+                                             quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+                                              sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam
+                                               quaerat voluptatem. Ut enim ad minima <br><br> veniam, quis nostrum exercitationem ullam 
+                                               corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem
+                                                vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+                                                 consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+                                    </div>
+                                </div>
 
 </div>
 
